808f34963fe4f4259c0c11ea59b37517acd8956c
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-03-31 23:19:03 +0530

    music player service

--- MiniMusicService.java
+++ MiniMusicService.java
@@ -51,3 +51,50 @@ public class MiniMusicPlayer
             ex.printStackTrace();
         }
     }
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick)
+    {
+        MidiEvent event = null;
+        try
+        {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd, chan, one, two);
+            event = new MidiEvent(a, tick);
+        }
+        catch(Exception e)
+        {}
+        return event;
+    }
+}
+
+class MyDrawPanel extends JPanel implements ControllerEventListener
+{
+    boolean msg = false;
+    public void controlChange(ShortMessage event)
+    {
+        msg=true;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        if(msg)
+        {
+            Graphics2D g2 = (Graphics2D) g;
+
+            int red = (int)(Math.random()*255);
+            int green = (int)(Math.random()*255);
+            int blue = (int)(Math.random()*255);
+
+            g.setColor(new Color(red,green,blue));
+
+            int ht = (int)((Math.random()*120)+10);
+            int wt = (int)((Math.random()*120)+10);
+
+            int x =(int)((Math.random()*40)+10);
+            int y =(int)((Math.random()*40)+10);
+
+            g.fillRect(x, y, wt, ht);
+            msg=false;
+        }
+    }
+}
